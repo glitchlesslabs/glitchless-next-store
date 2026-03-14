00df3d968abdb7754456e8c1b15f369c8ad250fb
cfe <cfe@glitchlesslabs.io>
2026-03-14 18:17:04 +0800

    chore: add featured products

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,4 +1,11 @@
-function FavoriteToggleButton() {
-  return <div>FavoriteToggleButton</div>;
+import { FaHeart } from 'react-icons/fa';
+import { Button } from '../ui/button';
+
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size="icon" variant="outline" className="cursor-pointer p-2">
+      <FaHeart />
+    </Button>
+  );
 }
 export default FavoriteToggleButton;
