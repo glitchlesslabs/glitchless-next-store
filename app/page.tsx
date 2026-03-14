a2126db67701d36b1903cc4d9ef024297ec4600a
cfe <cfe@glitchlesslabs.io>
2026-03-14 13:21:15 +0800

    chore: Shadcn change theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 function HomePage() {
   return (
     <div>
-      <h1 className="text-3xl">HomePage</h1>
+      <h1 className="text-3xl text-muted-foreground">HomePage</h1>
       <Button variant="outline" size="lg" className="m-8 capitalize">
         click me
       </Button>
