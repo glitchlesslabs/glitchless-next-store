1ac6f5c4c2f8583d91ea4e5926fd6ec15b141307
cfe <cfe@glitchlesslabs.io>
2026-03-14 12:39:12 +0800

    chore: add additional components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="m-8 capitalize">
+        click me
+      </Button>
+    </div>
+  );
 }
 export default HomePage;
