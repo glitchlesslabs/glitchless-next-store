f1bc0944df96c8e001e26e03402f9645660f8d5d
cfe <cfe@glitchlesslabs.io>
2026-03-14 20:12:19 +0800

    chore: setup the products container component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,19 @@
-function ProductsPage() {
-  return <div>ProductsPage</div>;
+import ProductsContainer from '@/components/products/ProductsContainer';
+
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}) {
+  const resolvedSearchParams = await searchParams;
+  const layoutParam = resolvedSearchParams.layout;
+  const searchParam = resolvedSearchParams.search;
+
+  const layout =
+    (Array.isArray(layoutParam) ? layoutParam[0] : layoutParam) || 'grid';
+  const search =
+    (Array.isArray(searchParam) ? searchParam[0] : searchParam) || '';
+
+  return <ProductsContainer layout={layout} search={search} />;
 }
 export default ProductsPage;
