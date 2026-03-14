0e58d7ac33d3c736941e62bebe67cd82216294e3
cfe <cfe@glitchlesslabs.io>
2026-03-14 20:22:14 +0800

    chore: display the products list component

--- components/products/ProductsList.tsx
+++ components/products/ProductsList.tsx
@@ -1,4 +1,49 @@
-function ProductsList() {
-  return <div>ProductsList</div>;
+import { formatCurrency } from '@/utils/format';
+import Link from 'next/link';
+import { Card, CardContent } from '@/components/ui/card';
+import { Product } from '@/generated/prisma/client';
+import Image from 'next/image';
+import FavoriteToggleButton from './FavoriteToggleButton';
+
+function ProductsList({ products }: { products: Product[] }) {
+  return (
+    <div className="mt-12 grid gap-y-8">
+      {products.map((product) => {
+        const { name, price, image, company } = product;
+        const dollarsAmount = formatCurrency(price);
+        const productId = product.id;
+        return (
+          <article key={productId} className="group relative">
+            <Link href={`/products/${productId}`}>
+              <Card className="transform py-0 transition-shadow group-hover:shadow-xl">
+                <CardContent className="grid gap-y-4 p-8 md:grid-cols-3">
+                  <div className="relative h-64 md:h-48 md:w-48">
+                    <Image
+                      src={image}
+                      alt={name}
+                      fill
+                      sizes="(max-width: 768px) 100vw,(max-width: 768px) 50vw, 33vw"
+                      priority
+                      className="w-full rounded object-cover"
+                    />
+                  </div>
+                  <div>
+                    <h2 className="text-xl font-semibold capitalize">{name}</h2>
+                    <h4 className="text-muted-foreground">{company}</h4>
+                  </div>
+                  <p className="text-lg text-muted-foreground md:ml-auto">
+                    {dollarsAmount}
+                  </p>
+                </CardContent>
+              </Card>
+            </Link>
+            <div className="absolute right-8 bottom-8 z-5">
+              <FavoriteToggleButton productId={productId} />
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
 export default ProductsList;
