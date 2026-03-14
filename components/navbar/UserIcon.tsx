93a6830796b619ef160643bf8b1439e6a7cb915a
cfe <cfe@glitchlesslabs.io>
2026-03-14 22:49:32 +0800

    chore: implement UserIcon component

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,16 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { LuUser } from 'react-icons/lu';
+import { currentUser } from '@clerk/nextjs/server';
+
+async function UserIcon() {
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img src={profileImage} className="h-6 w-6 rounded-full object-cover" />
+    );
+  }
+
+  return <LuUser className="h-6 w-6 rounded-full bg-primary text-white" />;
 }
 export default UserIcon;
