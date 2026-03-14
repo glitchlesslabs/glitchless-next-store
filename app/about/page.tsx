c05e42cb99df995a2dbac8e60f9b0edf147e19c0
cfe <cfe@glitchlesslabs.io>
2026-03-14 19:07:27 +0800

    chore: add loading container

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,19 @@
 function AboutPage() {
-  return <div>About Page</div>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap items-center justify-center gap-2 text-4xl leading-none font-bold tracking-wide sm:gap-x-6 sm:text-6xl">
+        We love
+        <span className="rounded-lg bg-primary px-4 py-2 tracking-widest text-white">
+          store
+        </span>
+      </h1>
+      <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 tracking-wide text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero hic
+        distinctio ducimus temporibus nobis autem laboriosam repellat, magni
+        fugiat minima excepturi neque, tenetur possimus nihil atque! Culpa nulla
+        labore nam?
+      </p>
+    </section>
+  );
 }
 export default AboutPage;
--- components/global/LoadingContainer.tsx
+++ components/global/LoadingContainer.tsx
@@ -1,4 +1,26 @@
+import { Card, CardContent } from '../ui/card';
+import { Skeleton } from '../ui/skeleton';
+
 function LoadingContainer() {
-  return <div>LoadingContainer</div>;
+  return (
+    <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3">
+      <LoadingProduct />
+      <LoadingProduct />
+      <LoadingProduct />
+    </div>
+  );
 }
+
+function LoadingProduct() {
+  return (
+    <Card>
+      <CardContent>
+        <Skeleton className="h-48 w-full" />
+        <Skeleton className="mt-4 h-4 w-3/4" />
+        <Skeleton className="mt-4 h-4 w-1/2" />
+      </CardContent>
+    </Card>
+  );
+}
+
 export default LoadingContainer;
