a00225d9423c2eb72937e4142a6068d587e10af3
cfe <cfe@glitchlesslabs.io>
2026-03-14 18:06:05 +0800

    chore: setup section title and empty list components

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,4 +1,13 @@
-function SectionTitle() {
-  return <div>SectionTitle</div>;
+import { Separator } from '../ui/separator';
+
+function SectionTitle({ text }: { text: string }) {
+  return (
+    <div>
+      <h2 className="mb-8 text-3xl font-medium tracking-wider capitalize">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 }
 export default SectionTitle;
