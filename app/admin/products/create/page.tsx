e659a149159369302332aca34bfdc094c03f777b
cfe <cfe@glitchlesslabs.io>
2026-03-18 11:10:27 +0800

    chore: initial setup for product page form and add faker library

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,4 +1,35 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { faker } from '@faker-js/faker';
+
+const createProductAction = async (formData: FormData) => {
+  'use server';
+
+  const name = formData.get('name') as string;
+  console.log(name);
+};
+
 function CreateProductPage() {
-  return <div>CreateProductPage</div>;
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
+
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold capitalize">create product</h1>
+      <div className="rounded-md border p-8">
+        <form action={createProductAction}>
+          <div className="mb-2">
+            <Label htmlFor="name">Product Name</Label>
+            <Input id="name" name="name" type="text" defaultValue={name} />
+          </div>
+          <Button type="submit" size="lg">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </section>
+  );
 }
 export default CreateProductPage;
