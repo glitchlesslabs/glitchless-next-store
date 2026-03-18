23a0aaa2a9611887bf58df9682cd5a0f9fe0564e
cfe <cfe@glitchlesslabs.io>
2026-03-18 12:20:49 +0800

    chore: implement createProductAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,21 @@
 'use server';
 
 import db from '@/utils/db';
+import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect('/');
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : 'An error occurred',
+  };
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -40,5 +54,31 @@ export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: 'product created' };
+  const user = await getAuthUser();
+  if (!user) redirect('/');
+  try {
+    const name = formData.get('name') as string;
+    const company = formData.get('company') as string;
+    const price = Number(formData.get('price') as string);
+    // temp
+    const image = formData.get('image') as File;
+    const description = formData.get('description') as string;
+    const featured = Boolean(formData.get('featured') as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: '/images/hero1.jpg',
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+
+    return { message: 'product created' };
+  } catch (error) {
+    return renderError(error);
+  }
 };
