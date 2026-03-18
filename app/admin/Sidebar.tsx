ed49597c446142f88391883127122d96dea2495c
cfe <cfe@glitchlesslabs.io>
2026-03-18 09:55:07 +0800

    chore: setup admin pages

---
+++ app/admin/Sidebar.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { adminLinks } from '@/utils/links';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+
+function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside>
+      {adminLinks.map((link) => {
+        const isActivePage = pathname === link.href;
+        const variant = isActivePage ? 'default' : 'ghost';
+        return (
+          <Button
+            asChild
+            className="mb-2 w-full font-normal capitalize"
+            key={link.href}
+            variant={variant}
+          >
+            <Link key={link.href} href={link.href}>
+              {link.label}
+            </Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
+}
+export default Sidebar;
