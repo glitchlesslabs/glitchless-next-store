32c4561e4fe8822969162ec807cf774014abae99
cfe <cfe@glitchlesslabs.io>
2026-03-18 21:17:08 +0800

    chore: Icon button and delete product action

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -41,3 +41,34 @@ export function SubmitButton({
     </Button>
   );
 }
+
+type actionType = 'edit' | 'delete';
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
+  const renderIcon = () => {
+    switch (actionType) {
+      case 'edit':
+        return <LuPen />;
+      case 'delete':
+        return <LuTrash2 />;
+      default:
+        const never: never = actionType;
+        throw new Error(`Invalid action type: ${never}`);
+    }
+  };
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="link"
+      className="cursor-pointer p-2"
+    >
+      {pending ? (
+        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+      ) : (
+        renderIcon()
+      )}
+    </Button>
+  );
+};
