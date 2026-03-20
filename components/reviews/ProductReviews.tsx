4c30a20760aace921475f83d6e340a06b4d34d8c
cfe <cfe@glitchlesslabs.io>
2026-03-20 11:31:38 +0800

    chore: product reviews component

--- components/reviews/ProductReviews.tsx
+++ components/reviews/ProductReviews.tsx
@@ -1,4 +1,27 @@
-function ProductReviews({ productId }: { productId: string }) {
-  return <div>ProductReviews</div>;
+import { fetchProductReviews } from '@/utils/actions';
+import SectionTitle from '../global/SectionTitle';
+import ReviewCard from './ReviewCard';
+
+async function ProductReviews({ productId }: { productId: string }) {
+  const reviews = await fetchProductReviews(productId);
+
+  return (
+    <div className="mt-16">
+      <SectionTitle text="product reviews" />
+      <div className="my-8 grid gap-8 md:grid-cols-2">
+        {reviews.map((review) => {
+          const { comment, rating, authorImageUrl, authorName } = review;
+          const reviewInfo = {
+            comment,
+            rating,
+            image: authorImageUrl,
+            name: authorName,
+          };
+
+          return <ReviewCard key={review.id} reviewInfo={reviewInfo} />;
+        })}
+      </div>
+    </div>
+  );
 }
 export default ProductReviews;
--- components/reviews/Rating.tsx
+++ components/reviews/Rating.tsx
@@ -2,7 +2,7 @@ import { FaStar, FaRegStar } from 'react-icons/fa';
 
 function Rating({ rating }: { rating: number }) {
   // rating = 2
-  const stars = Array.from({ length }, (_, i) => i + 1 <= rating);
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1 <= rating);
   return (
     <div className="flex items-center gap-x-1">
       {stars.map((isFilled, i) => {
