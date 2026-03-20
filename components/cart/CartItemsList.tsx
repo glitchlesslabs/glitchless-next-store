0c1de8d6f45fcd5c0e39ed3d807ce731dfbdc07e
cfe <cfe@glitchlesslabs.io>
2026-03-20 22:11:53 +0800

    chore: update the cart item list component

--- components/cart/CartItemsList.tsx
+++ components/cart/CartItemsList.tsx
@@ -1,4 +1,27 @@
-function CartItemsList() {
-  return <div>CartItemsList</div>;
+import { Card } from '@/components/ui/card';
+import { FirstColumn, SecondColumn, FourthColumn } from './CartItemColumns';
+import ThirdColumn from './ThirdColumn';
+import { CartItemWithProduct } from '@/utils/types';
+
+function CartItemsList({ cartItems }: { cartItems: CartItemWithProduct[] }) {
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        const { id, amount } = cartItem;
+        const { image, name, company, price, id: productId } = cartItem.product;
+        return (
+          <Card
+            key={id}
+            className="mb-8 flex flex-col flex-wrap gap-x-4 gap-y-4 p-6 md:flex-row"
+          >
+            <FirstColumn image={image} name={name} />
+            <SecondColumn name={name} company={company} productId={productId} />
+            <ThirdColumn />
+            <FourthColumn price={price} />
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
 export default CartItemsList;
